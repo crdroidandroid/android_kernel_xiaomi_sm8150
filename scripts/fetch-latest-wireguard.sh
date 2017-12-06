18498dc6b2e18f755b5b7f0ec35d3720b9b9f7c9
Jason A. Donenfeld <Jason@zx2c4.com>
2017-12-06 21:35:22 +0100

    net: wireguard: Add tools to import source automatically
    Change-Id: 3b6d84b4a78b0f6205f70863cda1e9537d727053
    Signed-off-by: Jason A. Donenfeld" <Jason@zx2c4.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

---
+++ scripts/fetch-latest-wireguard.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+USER_AGENT="WireGuard-AndroidROMBuild/0.3 ($(uname -a))"
+
+while read -r distro package version _; do
+	if [[ $distro == upstream && $package == linuxcompat ]]; then
+		VERSION="$version"
+		break
+	fi
+done < <(curl -A "$USER_AGENT" -LSs --connect-timeout 30 https://build.wireguard.com/distros.txt)
+
+[[ -n $VERSION ]]
+
+if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *$VERSION* ]]; then
+	touch net/wireguard/.check
+	exit 0
+fi
+
+rm -rf net/wireguard
+mkdir -p net/wireguard
+curl -A "$USER_AGENT" -LsS --connect-timeout 30 "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$VERSION.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "wireguard-linux-compat-$VERSION/src"
+sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
+touch net/wireguard/.check
