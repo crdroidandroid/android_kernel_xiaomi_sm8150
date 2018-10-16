426a7055c937da22b45f95155b61749aa1ec4288
Quentin Perret <quentin.perret@arm.com>
2018-10-16 11:14:59 +0100

    FROMLIST: sched: Relocate arch_scale_cpu_capacity
    By default, arch_scale_cpu_capacity() is only visible from within the
    kernel/sched folder. Relocate it to include/linux/sched/topology.h to
    make it visible to other clients needing to know about the capacity of
    CPUs, such as the Energy Model framework.
    
    Change-Id: I144c7299e122201dbcadc431d55d0a6d24d90005
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Message-Id: <20181016101513.26919-2-quentin.perret@arm.com>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -228,6 +228,17 @@ extern void set_sched_topology(struct sched_domain_topology_level *tl);
 # define SD_INIT_NAME(type)
 #endif
 
+#ifndef arch_scale_cpu_capacity
+static __always_inline
+unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
+{
+	if (sd && (sd->flags & SD_SHARE_CPUCAPACITY) && (sd->span_weight > 1))
+		return sd->smt_gain / sd->span_weight;
+
+	return SCHED_CAPACITY_SCALE;
+}
+#endif
+
 #else /* CONFIG_SMP */
 
 struct sched_domain_attr;
