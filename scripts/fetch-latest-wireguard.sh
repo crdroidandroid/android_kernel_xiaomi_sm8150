d29312087bb2dc7ee90f4f9f54fdb933fe15a55a
Felipe Leon <fglfgl27@gmail.com>
2019-03-08 15:08:20 -0300

    scripts/fetch-latest-wireguard.sh always update
    Change-Id: dd9da648896603ef8e87508ae3db4cbe0df2e209
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -9,16 +9,8 @@ while read -r distro package version _; do
 	fi
 done < <(curl -A "$USER_AGENT" -LSs --connect-timeout 30 https://build.wireguard.com/distros.txt)
 
-[[ -n $VERSION ]]
-
-if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *$VERSION* ]]; then
-	touch net/wireguard/.check
-	exit 0
-fi
-
 rm -rf net/wireguard
 mkdir -p net/wireguard
 curl -A "$USER_AGENT" -LsS --connect-timeout 30 "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$VERSION.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "wireguard-linux-compat-$VERSION/src"
 sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
-touch net/wireguard/.check
 git add net/wireguard && git commit -s --message="wireguard: Update to version ${VERSION}"
