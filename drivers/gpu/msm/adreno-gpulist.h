430b0509da3f4b0088f59ce8c3c01486a2657c3a
Danny Lin <danny@kdrag0n.dev>
2019-07-30 17:52:33 -0700

    msm: kgsl: Omit code for GPUs other than Adreno 640
    This kernel will not be used on devices with other GPUs.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -14,6 +14,7 @@
 #define ANY_ID (~0)
 
 static const struct adreno_gpu_core adreno_gpulist[] = {
+#if 0
 	{
 		.gpurev = ADRENO_REV_A306,
 		.core = 3,
@@ -414,6 +415,7 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.gpmu_major = 0x1,
 		.gpmu_minor = 0x008,
 	},
+#endif
 	{
 		.gpurev = ADRENO_REV_A640,
 		.core = 6,
@@ -458,6 +460,7 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.gpmu_tsens = 0x000C000D,
 		.max_power = 5448,
 	},
+#if 0
 	{
 		.gpurev = ADRENO_REV_A680,
 		.core = 6,
@@ -532,4 +535,5 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.busy_mask = 0xFFFFFFFE,
 		.cx_ipeak_gpu_freq = 900000000,
 	},
+#endif
 };
