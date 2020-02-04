7519731c1cbf286dbbf671a31b2a3a8cfef02481
Yury Norov <yury.norov@gmail.com>
2020-02-03 17:37:34 -0800

    lib: rework bitmap_parse()
    bitmap_parse() is ineffective and full of opaque variables and opencoded
    parts.  It leads to hard understanding and usage of it.  This rework
    includes:
    
    - remove bitmap_shift_left() call from the cycle.  Now it makes the
      complexity of the algorithm as O(nbits^2).  In the suggested approach
      the input string is parsed in reverse direction, so no shifts needed;
    
    - relax requirement on a single comma and no white spaces between
      chunks.  It is considered useful in scripting, and it aligns with
      bitmap_parselist();
    
    - split bitmap_parse() to small readable helpers;
    
    - make an explicit calculation of the end of input line at the
      beginning, so users of the bitmap_parse() won't bother doing this.
    
    Link: http://lkml.kernel.org/r/20200102043031.30357-6-yury.norov@gmail.com
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Cc: Amritha Nambiar <amritha.nambiar@intel.com>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Cc: "Tobin C . Harding" <tobin@kernel.org>
    Cc: Vineet Gupta <vineet.gupta1@synopsys.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/bitmap.h
+++ include/linux/bitmap.h
@@ -167,7 +167,7 @@ bitmap_find_next_zero_area(unsigned long *map,
 					      align_mask, 0);
 }
 
-extern int __bitmap_parse(const char *buf, unsigned int buflen, int is_user,
+extern int bitmap_parse(const char *buf, unsigned int buflen,
 			unsigned long *dst, int nbits);
 extern int bitmap_parse_user(const char __user *ubuf, unsigned int ulen,
 			unsigned long *dst, int nbits);
@@ -415,13 +415,7 @@ static inline void bitmap_shift_left(unsigned long *dst, const unsigned long *sr
 		__bitmap_shift_left(dst, src, shift, nbits);
 }
 
-static inline int bitmap_parse(const char *buf, unsigned int buflen,
-			unsigned long *maskp, int nmaskbits)
-{
-	return __bitmap_parse(buf, buflen, 0, maskp, nmaskbits);
-}
-
-/*
+/**
  * BITMAP_FROM_U64() - Represent u64 value in the format suitable for bitmap.
  *
  * Linux bitmaps are internally arrays of unsigned longs, i.e. 32-bit
