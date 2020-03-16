a1cf29e24a30fd0e201ffc00ccd6ea4aa2514049
Yaroslav Furman <yaro330@gmail.com>
2020-03-16 07:57:51 +0300

    mm: silence allocation warnings from dying processes
    Due to SLMKs semi-recent changes it started killing apps without waiting
    for them to allocate memory. This speeds up their deaths, but also
    tends to spam logs with alloc errors when slmk goes on its rampages.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4208,8 +4208,10 @@ retry:
 		goto nopage;
 
 	if (fatal_signal_pending(current) && !(gfp_mask & __GFP_NOFAIL) &&
-			(gfp_mask & __GFP_FS))
+			(gfp_mask & __GFP_FS)) {
+		gfp_mask |= __GFP_NOWARN;
 		goto nopage;
+	}
 
 	/* Try direct reclaim and then allocating */
 	if (!used_vmpressure)
