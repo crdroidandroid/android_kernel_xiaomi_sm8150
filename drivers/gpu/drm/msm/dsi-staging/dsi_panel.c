a8e9875c1dc17da938aaecc6f76f1d10ca3a7593
Demon000 <demonsingur@gmail.com>
2020-05-25 13:21:16 +0200

    drivers: gpu: drm: msm: dsi-staging: add dsi_panel_get_backlight
    Change-Id: Ifab7f9b0a7fbe5846c24c611a16cfcb7f29684dc

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -689,6 +689,11 @@ error:
 	return rc;
 }
 
+static u32 dsi_panel_get_backlight(struct dsi_panel *panel)
+{
+	return panel->bl_config.bl_level;
+}
+
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
