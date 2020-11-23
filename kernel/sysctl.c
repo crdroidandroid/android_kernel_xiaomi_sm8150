6161569975972d701f6fa9775bb732417b6252f1
kondors1995 <normandija1945@gmail.com>
2020-11-23 11:33:47 +0000

    sysctl: fix -Wunused-variable warning

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -137,7 +137,7 @@ static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_WALT
-static int two_million = 2000000;
+static __maybe_unused int two_million = 2000000;
 #endif
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
