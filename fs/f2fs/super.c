2e86dd247aa149901a042dfd6d7322d9baf50f26
Shachar Raindel <shacharr@gmail.com>
2020-11-28 09:41:24 -0800

    f2fs: Fix deadlock between f2fs_quota_sync and block_operation
    This deadlock is hitting Android users (Pixel 3/3a/4) with Magisk, due
    to frequent umount/mount operations that trigger quota_sync, hitting
    the race. See https://github.com/topjohnwu/Magisk/issues/3171 for
    additional impact discussion.
    
    In commit db6ec53b7e03, we added a semaphore to protect quota flags.
    As part of this commit, we changed f2fs_quota_sync to call
    f2fs_lock_op, in an attempt to prevent an AB/BA type deadlock with
    quota_sem locking in block_operation.  However, rwsem in Linux is not
    recursive. Therefore, the following deadlock can occur:
    
    f2fs_quota_sync
    down_read(cp_rwsem) // f2fs_lock_op
    filemap_fdatawrite
    f2fs_write_data_pages
    ...
                                       block_opertaion
    				   down_write(cp_rwsem) - marks rwsem as
    				                          "writer pending"
    down_read_trylock(cp_rwsem) - fails as there is
                                  a writer pending.
    			      Code keeps on trying,
    			      live-locking the filesystem.
    
    We solve this by creating a new rwsem, used specifically to
    synchronize this case, instead of attempting to reuse an existing
    lock.
    
    Signed-off-by: Shachar Raindel <shacharr@gmail.com>
    
    Fixes: db6ec53b7e03 f2fs: add a rw_sem to cover quota flag changes
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2766,7 +2766,36 @@ int f2fs_quota_sync(struct super_block *sb, int type)
 		if (!sb_has_quota_active(sb, cnt))
 			continue;
 
-		mapping = dqopt->files[cnt]->i_mapping;
+		if (!f2fs_sb_has_quota_ino(sbi))
+			inode_lock(dqopt->files[cnt]);
+
+		/*
+		 * do_quotactl
+		 *  f2fs_quota_sync
+		 *  f2fs_down_read(quota_sem)
+		 *  dquot_writeback_dquots()
+		 *  f2fs_dquot_commit
+		 *			      block_operation
+		 *			      f2fs_down_read(quota_sem)
+		 *
+		 * However, we cannot use the cp_rwsem to prevent this
+		 * deadlock, as the cp_rwsem is taken for read inside the
+		 * f2fs_dquot_commit code, and rwsem is not recursive.
+		 *
+		 * We therefore use a special lock to synchronize
+		 * f2fs_quota_sync with block_operations, as this is the only
+		 * place where such recursion occurs.
+		 */
+		f2fs_down_read(&sbi->cp_quota_rwsem);
+		f2fs_down_read(&sbi->quota_sem);
+
+		ret = f2fs_quota_sync_file(sbi, cnt);
+
+		f2fs_up_read(&sbi->quota_sem);
+		f2fs_up_read(&sbi->cp_quota_rwsem);
+
+		if (!f2fs_sb_has_quota_ino(sbi))
+			inode_unlock(dqopt->files[cnt]);
 
 		ret = filemap_fdatawrite(mapping);
 		if (ret)
@@ -4127,6 +4156,7 @@ try_onemore:
 	spin_lock_init(&sbi->stat_lock);
 	init_f2fs_rwsem(&sbi->cp_rwsem);
 	init_f2fs_rwsem(&sbi->quota_sem);
+	init_f2fs_rwsem(&sbi->cp_quota_rwsem);
 	init_waitqueue_head(&sbi->cp_wait);
 	spin_lock_init(&sbi->error_lock);
 
