5c8763d7099e8caf5541169d19c3067adad35c5f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-03 17:14:30 +0530

    drm: msm: Implement early fingerprint wake-up optimization
    Change-Id: Icfbc59fff5647b28631a670facc93ff837687d4c
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5580,6 +5580,7 @@ int dsi_display_dev_probe(struct platform_device *pdev)
 	display->pdev = pdev;
 	display->boot_disp = boot_disp;
 	display->dsi_type = dsi_type;
+	display->is_prim_display = true;
 #if defined(CONFIG_MACH_XIAOMI_VAYU) || defined(CONFIG_MACH_XIAOMI_NABU)
 	display->is_first_boot = true;
 #endif
