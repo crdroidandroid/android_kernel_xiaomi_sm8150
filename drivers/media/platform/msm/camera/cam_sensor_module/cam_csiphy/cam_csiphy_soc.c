411ee722735c0c37af6c2b2936c7f50c7423ded9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-03 21:33:14 +0530

    drivers: media: platform: msm: camera: Import Xiaomi changes
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    
    Change-Id: I20a82c9e77f92405d77a0d16621ce12209dfd194

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
@@ -12,7 +12,11 @@
 
 #include "cam_csiphy_soc.h"
 #include "cam_csiphy_core.h"
+#ifdef CONFIG_MACH_XIAOMI_VAYU
+#include "include/cam_csiphy_1_1_hwreg_vayu.h"
+#else
 #include "include/cam_csiphy_1_1_hwreg.h"
+#endif
 #include "include/cam_csiphy_1_0_hwreg.h"
 #include "include/cam_csiphy_1_2_hwreg.h"
 #include "include/cam_csiphy_1_2_2_hwreg.h"
@@ -254,7 +258,12 @@ int32_t cam_csiphy_parse_dt_info(struct platform_device *pdev,
 		csiphy_dev->is_csiphy_3phase_hw = CSI_3PHASE_HW;
 		csiphy_dev->hw_version = CSIPHY_VERSION_V11;
 		csiphy_dev->clk_lane = 0;
+#ifdef CONFIG_MACH_XIAOMI_SM8150
+		csiphy_dev->ctrl_reg->data_rates_settings_table =
+			&data_rate_delta_table_1_1;
+#else
 		csiphy_dev->ctrl_reg->data_rates_settings_table = NULL;
+#endif
 	} else if (of_device_is_compatible(soc_info->dev->of_node,
 		"qcom,csiphy-v1.2")) {
 		csiphy_dev->ctrl_reg->csiphy_2ph_reg = csiphy_2ph_v1_2_reg;
