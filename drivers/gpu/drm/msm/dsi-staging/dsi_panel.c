665dd1be0813fd54b543af78f37ec4bd5ccf4b57
Art_Chen <Chenxy0201@qq.com>
2021-01-26 01:59:10 +0800

    WIP:disp: msm: Implement fod dim layer sysfs node
     * The problem with previous commits was that those intended to enable
       dim layer and HBM simultaneously each time the icon was pressed,
       inducing a screen flashing issue.
     * Fix the issue by adding a sysfs node to make sure dim layer is shown
       before HBM is enabled.
    
    Change-Id: I630df887c1d84c61492dd1af74954d493c4aa9a5
    Signed-off-by: Art_Chen <Chenxy0201@qq.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -725,6 +725,8 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 			panel->fod_dim_lut[i - 1].alpha, panel->fod_dim_lut[i].alpha);
 }
 
+extern bool is_dimlayer_hbm_enabled;
+
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
 	int rc = 0;
@@ -4481,6 +4483,8 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	else
 		panel->panel_initialized = true;
 	mutex_unlock(&panel->panel_lock);
+
+	dsi_panel_set_fod_hbm(panel, is_dimlayer_hbm_enabled);
 
 	if (panel->hbm_mode)
 		dsi_panel_apply_hbm_mode(panel);
