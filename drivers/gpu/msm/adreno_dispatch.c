5e6376c9205a7a8079062c3fede2e2da5a58d0be
nato6613 <normandija1945@gmail.com>
2021-07-25 11:59:57 +0000

    drivers: gpu: msm: silence unused variables warning

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -2297,7 +2297,7 @@ static void retire_cmdobj(struct adreno_device *adreno_dev,
 		struct kgsl_drawobj_cmd *cmdobj)
 {
 	struct kgsl_drawobj *drawobj = DRAWOBJ(cmdobj);
-	struct adreno_context *drawctxt = ADRENO_CONTEXT(drawobj->context);
+	struct adreno_context __maybe_unused *drawctxt = ADRENO_CONTEXT(drawobj->context);
 	uint64_t start = 0, end = 0;
 
 	if (cmdobj->fault_recovery != 0) {
