4613e9e41d50221e7b1372affa2e183aee9917c7
kondors1995 <normandija1945@gmail.com>
2021-12-10 14:06:20 +0000

    Revert "fs: fuse: Import fuse_shortcircuit from Oxygen OS 11.2.1.1.LE28DA"
    This reverts commit df29162402c26b6996dbcd47e88841486ef33651.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1393,8 +1393,6 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
-	struct fuse_package *fpack;
-
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1404,12 +1402,6 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
-};
-
-struct fuse_package {
-	bool fuse_open_req;
-	struct file *filp;
-	char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
