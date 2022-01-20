f79cb696f5c57e4bcb3eea93b5bbdd026cdd92b9
kondors1995 <normandija1945@gmail.com>
2022-01-20 11:08:54 +0000

    Revert "rpmsg: glink: Append glink irq name with remote edge name"
    This reverts commit 6679a153b69ab73d6cf261b01533008e58fdbdd1.

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -142,7 +142,6 @@ struct qcom_glink {
 	struct qcom_glink_pipe *tx_pipe;
 
 	int irq;
-	char irqname[GLINK_NAME_SIZE];
 
 	struct kthread_worker kworker;
 	struct task_struct *task;
@@ -2035,8 +2034,6 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	if (ret)
 		dev_err(dev, "failed to register early notif %d\n", ret);
 
-	snprintf(glink->irqname, 32, "glink-native-%s", glink->name);
-
 	irq = of_irq_get(dev->of_node, 0);
 
 	/* Use different irq flag option in case of gvm */
@@ -2049,7 +2046,7 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	ret = devm_request_irq(dev, irq,
 			       qcom_glink_native_intr,
 			       irqflags,
-			       glink->irqname, glink);
+			       "glink-native", glink);
 	if (ret) {
 		dev_err(dev, "failed to request IRQ\n");
 		goto unregister;
