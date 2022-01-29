12a1f0ada023abaacb3467705888f36a5b39ebcc
Luan Cachoroski Halaiko <luhalaiko@gmail.com>
2022-01-29 14:58:19 -0300

    Revert "fingerprint: fpc1020_tee: Switch to msm drm notifier api"
    This reverts commit 40882ccb002e3fe905521838acc969f554a628f5.

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -37,7 +37,7 @@
 #include <linux/pm_wakeup.h>
 #include <linux/fb.h>
 #include <drm/drm_bridge.h>
-#include <linux/msm_drm_notify.h>
+#include <drm/drm_notifier.h>
 
 #define FPC_SCREEN_HOLD_TIME 2000
 #define FPC_TTW_HOLD_TIME 2000
@@ -648,28 +648,28 @@ static int fpc_fb_notif_callback(struct notifier_block *nb,
 {
 	struct fpc1020_data *fpc1020 = container_of(nb, struct fpc1020_data,
 			fb_notifier);
-	struct msm_drm_notifier *evdata = data;
+	struct fb_event *evdata = data;
 	unsigned int blank;
 
 	if (!fpc1020)
 		return 0;
 
-	if (val != MSM_DRM_EVENT_BLANK || fpc1020->prepared == false)
+	if (val != DRM_EVENT_BLANK || fpc1020->prepared == false)
 		return 0;
 
 	pr_debug("[info] %s value = %d\n", __func__, (int)val);
 
-	if (evdata && evdata->data && val == MSM_DRM_EVENT_BLANK) {
+	if (evdata && evdata->data && val == DRM_EVENT_BLANK) {
 		blank = *(int *)(evdata->data);
 		switch (blank) {
-		case MSM_DRM_BLANK_POWERDOWN:
+		case DRM_BLANK_POWERDOWN:
 			fpc1020->fb_black = true;
 #ifdef CONFIG_FINGERPRINT_FPC_SCREEN_NOTIFY
 			__pm_wakeup_event(&fpc1020->screen_wl, FPC_SCREEN_HOLD_TIME);
 			sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_screen_status.attr.name);
 #endif
 			break;
-		case MSM_DRM_BLANK_UNBLANK:
+		case DRM_BLANK_UNBLANK:
 			fpc1020->fb_black = false;
 #ifdef CONFIG_FINGERPRINT_FPC_SCREEN_NOTIFY
 			__pm_wakeup_event(&fpc1020->screen_wl, FPC_SCREEN_HOLD_TIME);
@@ -770,7 +770,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 	fpc1020->wait_finger_down = false;
 	INIT_WORK(&fpc1020->work, notification_work);
 	fpc1020->fb_notifier = fpc_notif_block;
-	msm_drm_register_client(&fpc1020->fb_notifier);
+	drm_register_client(&fpc1020->fb_notifier);
 
 	dev_info(dev, "%s: ok\n", __func__);
 
@@ -782,7 +782,7 @@ static int fpc1020_remove(struct platform_device *pdev)
 {
 	struct fpc1020_data *fpc1020 = platform_get_drvdata(pdev);
 
-	msm_drm_unregister_client(&fpc1020->fb_notifier);
+	drm_unregister_client(&fpc1020->fb_notifier);
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
 	wakeup_source_trash(&fpc1020->ttw_wl);
