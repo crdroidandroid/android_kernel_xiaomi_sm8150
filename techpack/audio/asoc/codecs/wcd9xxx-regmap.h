9f9d386fe798e05e6d53ebf6b0cc02f214564056
Luan Cachoroski Halaiko <luhalaiko@gmail.com>
2022-02-10 20:31:38 -0300

    techpack: audio: do not compile non defined drivers --> Decreases kernel size and speeds up compilation
    Signed-off-by: Luan Cachoroski Halaiko <luhalaiko@gmail.com>

--- techpack/audio/asoc/codecs/wcd9xxx-regmap.h
+++ techpack/audio/asoc/codecs/wcd9xxx-regmap.h
@@ -19,30 +19,38 @@
 
 typedef int (*regmap_patch_fptr)(struct regmap *, int);
 
+#ifdef CONFIG_SND_SOC_WCD9360
 extern struct regmap_config wcd9360_regmap_config;
+#endif
 
 extern struct regmap_config wcd934x_regmap_config;
 extern int wcd934x_regmap_register_patch(struct regmap *regmap,
 					 int version);
 
+#ifdef CONFIG_SND_SOC_WCD9335
 extern struct regmap_config wcd9335_regmap_config;
 extern int wcd9335_regmap_register_patch(struct regmap *regmap,
 					 int version);
+#endif
 
 static inline struct regmap_config *wcd9xxx_get_regmap_config(int type)
 {
 	struct regmap_config *regmap_config;
 
 	switch (type) {
+#ifdef CONFIG_SND_SOC_WCD9360
 	case WCD9360:
 		regmap_config = &wcd9360_regmap_config;
 		break;
+#endif
 	case WCD934X:
 		regmap_config = &wcd934x_regmap_config;
 		break;
+#ifdef CONFIG_SND_SOC_WCD9335
 	case WCD9335:
 		regmap_config = &wcd9335_regmap_config;
 		break;
+#endif
 	default:
 		regmap_config = NULL;
 		break;
@@ -56,9 +64,11 @@ static inline regmap_patch_fptr wcd9xxx_get_regmap_reg_patch(int type)
 	regmap_patch_fptr apply_patch;
 
 	switch (type) {
+#ifdef CONFIG_SND_SOC_WCD9335
 	case WCD9335:
 		apply_patch = wcd9335_regmap_register_patch;
 		break;
+#endif
 	case WCD934X:
 		apply_patch = wcd934x_regmap_register_patch;
 		break;
