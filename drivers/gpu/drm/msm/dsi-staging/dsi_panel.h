a43a49104fda5b83244c3c34fb3aceb065ae308f
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:15:59 +0200

    display: dsi_panel: expose FOD dim
    Change-Id: I439f536efefb7b338462ccf6d03b078cea76c1c3
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -243,6 +243,8 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	unsigned int fod_dim_lut_len;
+	u8 fod_dim_alpha;
+	bool fod_hbm_enabled;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -366,5 +368,10 @@ void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 u8 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
+void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
+		struct dsi_display_mode *mode, u32 frame_threshold_us);
+
+int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
 #endif /* _DSI_PANEL_H_ */
