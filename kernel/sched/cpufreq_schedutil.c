941b37c3ece51c099b79959e2cdb547e98c3e4a5
Luan Cachoroski Halaiko <luhalaiko@gmail.com>
2022-02-16 13:51:52 -0300

    kernel: sched: fix compilation for schedutil
    Signed-off-by: Luan Cachoroski Halaiko <luhalaiko@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -189,10 +189,6 @@ static void sugov_deferred_update(struct sugov_policy *sg_policy, u64 time,
 {
 	if (!sugov_update_next_freq(sg_policy, time, next_freq))
 		return;
-
-	if (use_pelt())
-		sg_policy->work_in_progress = true;
-	sched_irq_work_queue(&sg_policy->irq_work);
 }
 
 /**
