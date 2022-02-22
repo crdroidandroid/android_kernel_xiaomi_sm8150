7e21330d1c2506df0a06838545158f591228a644
Luan Cachoroski Halaiko <luhalaiko@gmail.com>
2022-02-22 16:15:51 -0300

    dsi: msm: add a switch to force enable display features --> If features can't be enabled in the display drivers in arch/     use this switch to force the use of ULPS and PHY poweroff.
    Signed-off-by: Luan Cachoroski Halaiko <luhalaiko@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2224,6 +2224,10 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	struct dsi_parser_utils *utils = &panel->utils;
 	u32 val;
 
+#ifdef CONFIG_DSI_FEATURES
+	panel->ulps_feature_enabled = true;
+	panel->ulps_suspend_enabled = true;
+#else
 	panel->ulps_feature_enabled =
 		utils->read_bool(utils->data, "qcom,ulps-enabled");
 
@@ -2235,6 +2239,7 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
+#endif
 
 	panel->te_using_watchdog_timer = utils->read_bool(utils->data,
 					"qcom,mdss-dsi-te-using-wd");
