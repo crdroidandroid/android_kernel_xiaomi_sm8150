afe82bb8a18f438234ba84ac433c39c1313f6021
kondors1995 <normandija1945@gmail.com>
2022-04-11 08:56:27 +0000

    drivers:md:Fix -Wunused-variable warning
    ../drivers/md/dm.c:1534:6: warning: unused variable 'rw' [-Wunused-variable]
            int rw = bio_data_dir(bio);  AR      drivers/media/pci/pt1/built-in.o

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1531,7 +1531,7 @@ static void __split_and_process_bio(struct mapped_device *md,
  */
 static blk_qc_t dm_make_request(struct request_queue *q, struct bio *bio)
 {
-	int rw = bio_data_dir(bio);
+	int rw __maybe_unused = bio_data_dir(bio);
 	struct mapped_device *md = q->queuedata;
 	int srcu_idx;
 	struct dm_table *map;
