79d125c52921eaf7cd1fa59c488ec36fb1db89a3
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-13 09:33:09 +0300

    display: expose forcing FOD UI for debugging
    Change-Id: I1f1b5344c9f98b541dc5c99e5d6f9c5560d8340c
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -247,6 +247,7 @@ struct dsi_panel {
 	u8 fod_dim_alpha;
 	bool fod_hbm_enabled;
 	bool fod_ui;
+	bool force_fod_ui;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -377,5 +378,6 @@ void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
+bool dsi_panel_get_force_fod_ui(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
