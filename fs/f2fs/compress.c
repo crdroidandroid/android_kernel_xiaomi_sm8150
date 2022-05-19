5dc690a12f48a09f1ed57bedd52b4b7dc5db31ee
kondors1995 <normandija1945@gmail.com>
2022-05-19 09:31:00 +0000

    f2fs:remove cold parameter for pagevecs

--- fs/f2fs/compress.c
+++ fs/f2fs/compress.c
@@ -1815,7 +1815,7 @@ void f2fs_invalidate_compress_pages(struct f2fs_sb_info *sbi, nid_t ino)
 	if (!mapping->nrpages)
 		return;
 
-	pagevec_init(&pvec, 0);
+	pagevec_init(&pvec);
 
 	do {
 		unsigned int nr_pages;
