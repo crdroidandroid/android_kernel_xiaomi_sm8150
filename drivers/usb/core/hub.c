a41c1ff44901e1f13641990e6468ae9fc736c1ce
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-22 19:46:21 +0200

    drivers: usb: Import Xiaomi changes
    Change-Id: I0c11d47f789ffe489a08c9149e94a39c3bad27f0
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4825,7 +4825,9 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
+#ifndef CONFIG_MACH_XIAOMI_SM8150 /*disable USB 2.0 Link PM to fix long time usb storage recognition issue*/
 	hub_set_initial_usb2_lpm_policy(udev);
+#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
