5261716c94f0d7a9a4ed7e5e4096a19589481579
John Galt <johngaltfirstrun@gmail.com>
2022-07-01 09:05:25 -0400

    mm/vmscan: swappiness inline with init
    On our device, we set 100 at init and allow userspace to write
    swappiness. Let's just set early so we don't need to wait for init
    scripts to run.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -168,7 +168,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 20;
+int vm_swappiness = 100;
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
  * Direct reclaim swappiness, values range from 0 .. 60. Higher means more swappy.
