c7367162fde046cab552d6a9bca1ffc3deb50d00
Andrzej Perczak <linux@andrzejperczak.com>
2022-07-05 23:25:51 +0200

    uclamp_assist: Tune parameters

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2128,13 +2128,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 0, 1},
-		{"foreground", 	     	"0",  "50",  1, 1},
-		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "30",  0, 0},
-		{"camera-daemon",	"50", "100", 1, 1},
+		{"top-app",    	     	"20", "max", 1, 1},  // 20-100%
+		{"foreground", 	     	"10", "80",  1, 0},  // 10-80%
+		{"background", 	     	"0",  "50",  0, 0},  // 0-50%
+		{"system-background", 	"0",  "60",  0, 0},  // 0-60%
+		{"restricted",          "0",  "20",  0, 0},  // 0-20%
 		{"display",             "50", "100", 1, 1},
-		{"restricted", 		"0", "30", 0, 0},
+		{"camera-daemon",       "20", "max", 1, 1},  // 20-100%
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
