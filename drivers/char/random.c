55d14a6997be397a40d7a50ee1a849b56b5443d0
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-07-06 23:37:40 +0800

    Revert "char: random: fix unused function error"
    This reverts commit 2e9852f8f3846b9daa3d9616672d654cdd227554.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1033,7 +1033,6 @@ static void crng_backtrack_protect(__u8 tmp[CHACHA_BLOCK_SIZE], int used)
 	_crng_backtrack_protect(select_crng(), tmp, used);
 }
 
-#ifndef CONFIG_SRANDOM
 static ssize_t extract_crng_user(void __user *buf, size_t nbytes)
 {
 	ssize_t ret = 0, i = CHACHA_BLOCK_SIZE;
@@ -1068,7 +1067,7 @@ static ssize_t extract_crng_user(void __user *buf, size_t nbytes)
 
 	return ret;
 }
-#endif
+
 
 /*********************************************************************
  *
