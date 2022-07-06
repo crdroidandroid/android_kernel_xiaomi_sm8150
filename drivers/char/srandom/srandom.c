9870fb2487af4edd1176226f1c4dde2169ec8c37
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-07-06 23:37:44 +0800

    Revert "char: srandom: expose sdevice_{read,write} and sfops for treewide usage"
    This reverts commit a7409d555e3bf537cc73145bc5e9449fa2c487e0.

--- drivers/char/srandom/srandom.c
+++ drivers/char/srandom/srandom.c
@@ -12,7 +12,6 @@
 #include <linux/mutex.h>
 #include <linux/delay.h>
 #include <linux/kthread.h>
-#include "srandom.h"
 
 #define DRIVER_AUTHOR "Jonathan Senkerik <josenk@jintegrate.co>"
 #define DRIVER_DESC   "Improved random number generator."
@@ -65,7 +64,8 @@
  */
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
-
+static ssize_t sdevice_read(struct file *, char *, size_t, loff_t *);
+static ssize_t sdevice_write(struct file *, const char *, size_t, loff_t *);
 static uint64_t xorshft64(void);
 static uint64_t xorshft128(void);
 static int nextbuffer(void);
@@ -81,7 +81,7 @@ static int work_thread(void *data);
 /*
  * Global variables are declared as static, so are global within the file.
  */
-const struct file_operations sfops = {
+static struct file_operations sfops = {
         .owner   = THIS_MODULE,
         .open    = device_open,
         .read    = sdevice_read,
@@ -284,7 +284,7 @@ static int device_release(struct inode *inode, struct file *file)
 /*
  * Called when a process reads from the device.
  */
-ssize_t sdevice_read(struct file * file, char * buf, size_t requestedCount, loff_t *ppos)
+static ssize_t sdevice_read(struct file * file, char * buf, size_t requestedCount, loff_t *ppos)
 {
         int arraysPosition;
         int Block, ret;
@@ -375,12 +375,11 @@ ssize_t sdevice_read(struct file * file, char * buf, size_t requestedCount, loff
         return requestedCount;
 }
 
-EXPORT_SYMBOL(sdevice_read);
 
 /*
  * Called when someone tries to write to /dev/srandom device
  */
-ssize_t sdevice_write(struct file *file, const char __user *buf, size_t receivedCount, loff_t *ppos)
+static ssize_t sdevice_write(struct file *file, const char __user *buf, size_t receivedCount, loff_t *ppos)
 {
 
         char *newdata;
@@ -412,7 +411,6 @@ ssize_t sdevice_write(struct file *file, const char __user *buf, size_t received
         return receivedCount;
 }
 
-EXPORT_SYMBOL(sdevice_write);
 
 
 /*
