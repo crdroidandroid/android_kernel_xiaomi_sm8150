506627b450a18d922ed57778623e8edcfdd01034
kondors1995 <normandija1945@gmail.com>
2022-07-08 11:22:38 +0000

    Revert "Revert "drm/msm/dsi-staging: Allow override the panel framerate"" Lets keep it as fallback for roms with dont support smooth display
    This reverts commit 9235e8c0dfdf5ede4c3bbe3541a41052e7aae90d.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  *
@@ -913,6 +914,9 @@ error:
 	return rc;
 }
 
+static unsigned int framerate_override;
+module_param(framerate_override, uint, 0444);
+
 static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 				  struct dsi_parser_utils *utils)
 {
@@ -935,6 +939,18 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 	}
 
 	mode->clk_rate_hz = !rc ? tmp64 : 0;
+	if (tmp64 == 1100000000 || tmp64 == 1103000000) {
+        if (framerate_override == 5)
+			mode->clk_rate_hz = 1485000000; // 81hz
+		else if (framerate_override == 4)
+			mode->clk_rate_hz = 1375000000; // 75hz
+		else if (framerate_override == 3)
+			mode->clk_rate_hz = 1320000000; // 72hz
+		else if (framerate_override == 2)
+			mode->clk_rate_hz = 1265000000; // 69hz
+		else if (framerate_override == 1)
+			mode->clk_rate_hz = 1210000000; // 66hz
+	}
 	display_mode->priv_info->clk_rate_hz = mode->clk_rate_hz;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-mdp-transfer-time-us",
@@ -958,6 +974,18 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (mode->refresh_rate == 60) {
+        if (framerate_override == 5)
+			mode->refresh_rate = 81;
+		else if (framerate_override == 4)
+			mode->refresh_rate = 75;
+		else if (framerate_override == 3)
+			mode->refresh_rate = 72;
+		else if (framerate_override == 2)
+			mode->refresh_rate = 69;
+		else if (framerate_override == 1)
+			mode->refresh_rate = 66;
+	}
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-panel-width",
 				  &mode->h_active);
@@ -974,6 +1002,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->h_front_porch = 32;
 
 	rc = utils->read_u32(utils->data,
 				"qcom,mdss-dsi-h-back-porch",
@@ -983,6 +1013,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->h_back_porch = 16;
 
 	rc = utils->read_u32(utils->data,
 				"qcom,mdss-dsi-h-pulse-width",
@@ -992,6 +1024,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->h_sync_width = 16;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-h-sync-skew",
 				  &mode->h_skew);
@@ -1017,6 +1051,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->v_back_porch = 16;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-v-front-porch",
 				  &mode->v_front_porch);
@@ -1025,6 +1061,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->v_front_porch = 8;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-v-pulse-width",
 				  &mode->v_sync_width);
@@ -1033,6 +1071,8 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	if (framerate_override)
+		mode->v_sync_width = 8;
 	pr_debug("panel vert active:%d front_portch:%d back_porch:%d pulse_width:%d\n",
 		mode->v_active, mode->v_front_porch, mode->v_back_porch,
 		mode->v_sync_width);
