4940d77039a94c4a1f0372763234db792d7fbf73
Rohit Gupta <rohgup@codeaurora.org>
2022-09-16 19:22:24 +0000

    cpufreq: schedutil: Cache tunables on governor exit Currently when all the related CPUs from a policy go offline or the governor is switched, cpufreq framework calls sugov_exit() that frees the governor tunables. When any of the related CPUs comes back online or governor is switched back to schedutil sugov_init() gets called which allocates a fresh set of tunables that are set to default values. This can cause the userspace settings to those tunables to be lost across governor switches or when an entire cluster is hotplugged out. To prevent this, save the tunable values on governor exit. Restore these values to the newly allocated tunables on governor init.
    Change-Id: I671d4d0e1a4e63e948bfddb0005367df33c0c249
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    [avilaj@codeaurora.org: Modify to handle new tunables on 4.19]
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    [onettboots: Add missing from cherry-pick]
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -855,8 +855,10 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count)
+	if (!count) {
+		sugov_tunables_save(policy, tunables);
 		sugov_clear_global_tunables();
+	}
 
 	mutex_unlock(&global_tunables_lock);
 
