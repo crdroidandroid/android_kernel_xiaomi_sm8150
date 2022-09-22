a3661e92fb30e15fa13e080adf9dadfd6732de62
kondors1995 <normandija1945@gmail.com>
2022-09-22 15:34:19 +0300

    build.sh:LLVM=1

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ restore='\033[0m'
 clear
 
 # Resources
-export CLANG_PATH=/datadrive/sov/prebuilts/clang/host/linux-x86/clang-trb/bin
+export CLANG_PATH=~/trb_clang/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CROSS_COMPILE=${CLANG_PATH}/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=${CLANG_PATH}/arm-linux-gnueabi-
-export THINLTO_CACHE=/datadrive/kernel/ltocache/
+export THINLTO_CACHE=~/ltocache/
 DEFCONFIG="raphael_defconfig"
 
 # Kernel Details
@@ -21,8 +21,8 @@ VER="R3.5-SYB"
 
 # Paths
 KERNEL_DIR=`pwd`
-REPACK_DIR=/datadrive/AnyKernel3
-ZIP_MOVE=/datadrive/AK-releases
+REPACK_DIR=~/AnyKernel3
+ZIP_MOVE=~/AK-releases
 
 # Functions
 function clean_all {
@@ -34,8 +34,8 @@ function clean_all {
 
 function make_kernel {
 		echo
-		make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip $DEFCONFIG
-		make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(grep -c ^processor /proc/cpuinfo)
+		make LLVM=1 LLVM_IAS=1 $DEFCONFIG
+		make LLVM=1 LLVM_IAS=1 -j$(grep -c ^processor /proc/cpuinfo)
 
 }
 
