56c7022e543ef11650c4ce1a64c13290f6f065e7
Signed-off-by: Wei Wang <wvw@google.com>
2022-10-03 16:37:03 +0000

    sched: restrict iowait boost to tasks with prefer_idle Currently iowait doesn't distinguish background/foreground tasks and we have seen cases where a device run to high frequency unnecessarily when running some background I/O. This patch limits iowait boost to tasks with prefer_idle only. Specifically, on Pixel, those are foreground and top app tasks.
    Bug: 130308826
    Test: Boot and trace
    Change-Id: I2d892beeb4b12b7e8f0fb2848c23982148648a10
    Signed-off-by: Wei Wang <wvw@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5273,7 +5273,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 #elif  CONFIG_UCLAMP_TASK
 				(uclamp_latency_sensitive(p) > 0) : 0;
 #endif
-	int idle_h_nr_running = idle_policy(p->policy);
 
 #ifdef CONFIG_SCHED_WALT
 	p->misfit = !task_fits_max(p, rq->cpu);
@@ -5309,7 +5308,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	 * utilization updates, so do it here explicitly with the IOWAIT flag
 	 * passed.
 	 */
-	if (p->in_iowait)
+	if (p->in_iowait && prefer_idle)
 		cpufreq_update_util(rq, SCHED_CPUFREQ_IOWAIT);
 
 	for_each_sched_entity(se) {
@@ -5347,7 +5346,14 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	if (!se) {
 		add_nr_running(rq, 1);
 		inc_rq_walt_stats(rq, p);
-		if (!task_new)
+		/*
+		 * If the task prefers idle cpu, and it also is the first
+		 * task enqueued in this runqueue, then we don't check
+		 * overutilized. Hopefully the cpu util will be back to
+		 * normal before next overutilized check.
+		 */
+		if (!task_new &&
+			!(prefer_idle && rq->nr_running == 1))
 			update_overutilized_status(rq);
 	}
 
