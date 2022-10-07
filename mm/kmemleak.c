f71644442b766309965c753e244fe0a83de60c17
kondors1995 <normandija1945@gmail.com>
2022-10-07 11:50:20 +0300

    Revert "mm: kmemleak: Don't die when memory allocation fails"
    This reverts commit 80edee2cd7c874a4137927be4a5e2f96be337ced.

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -571,10 +571,11 @@ static struct kmemleak_object *create_object(unsigned long ptr, size_t size,
 	struct rb_node **link, *rb_parent;
 	unsigned long untagged_ptr;
 
-	while (1) {
-		object = kmem_cache_alloc(object_cache, gfp_kmemleak_mask(gfp));
-		if (object)
-			break;
+	object = kmem_cache_alloc(object_cache, gfp_kmemleak_mask(gfp));
+	if (!object) {
+		pr_warn("Cannot allocate a kmemleak_object structure\n");
+		kmemleak_disable();
+		return NULL;
 	}
 
 	INIT_LIST_HEAD(&object->object_list);
