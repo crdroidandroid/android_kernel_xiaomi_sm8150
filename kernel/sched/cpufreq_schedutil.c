ef56666ec48ab27789444cd850a6ee2cb0c4ad26
mcdofrenchfreis <xyzevan@androidist.net>
2022-12-14 09:51:38 +0800

    cpufreq: schedutil: hardcode ratelimits and apply init protection
    up_rate_limit: 0
    down_rate_limit: 0
    
    CAF Defaults
    
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -803,12 +803,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	tunables->down_rate_limit_us =
-				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-
 	tunables->iowait_boost_enable = true;
+   /* Hardcode ratelimits with CAF values */
+	tunables->up_rate_limit_us = 0;
+	tunables->down_rate_limit_us = 0;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
