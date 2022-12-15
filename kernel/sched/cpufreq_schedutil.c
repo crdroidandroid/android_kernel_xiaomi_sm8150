1078656c377c1fc3c5fc935464cf2f6ba75640b0
onettboots <blackcocopet@gmail.com>
2022-12-15 09:52:32 +0000

    Revert "cpufreq: schedutil: Cache tunables on governor exit"
    This reverts commit 7f1fb83aa39323f27d7f491741f7c8e6c06f28af.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -855,10 +855,8 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count) {
-		sugov_tunables_save(policy, tunables);
+	if (!count)
 		sugov_clear_global_tunables();
-	}
 
 	mutex_unlock(&global_tunables_lock);
 
