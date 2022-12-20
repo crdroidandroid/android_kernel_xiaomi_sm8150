fdbb12feac2543465b9428b78a47edb94763d3d3
John Galt <johngaltfirstrun@gmail.com>
2022-12-20 09:52:16 -0500

    sde_plane: correctly set PLANE_PROP_FOD to dirty rects
    [pwnrazr:we use PLANE_PROP_FOD in raphael]
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_plane.c
+++ drivers/gpu/drm/msm/sde/sde_plane.c
@@ -4149,6 +4149,7 @@ static int sde_plane_sspp_atomic_update(struct drm_plane *plane,
 		case PLANE_PROP_V_DECIMATE:
 		case PLANE_PROP_SRC_CONFIG:
 		case PLANE_PROP_ZPOS:
+		case PLANE_PROP_FOD:
 		case PLANE_PROP_EXCL_RECT_V1:
 			pstate->dirty |= SDE_PLANE_DIRTY_RECTS;
 			break;
@@ -4166,7 +4167,6 @@ static int sde_plane_sspp_atomic_update(struct drm_plane *plane,
 		case PLANE_PROP_ALPHA:
 		case PLANE_PROP_INPUT_FENCE:
 		case PLANE_PROP_BLEND_OP:
-		case PLANE_PROP_FOD:
 			/* no special action required */
 			break;
 		case PLANE_PROP_FB_TRANSLATION_MODE:
