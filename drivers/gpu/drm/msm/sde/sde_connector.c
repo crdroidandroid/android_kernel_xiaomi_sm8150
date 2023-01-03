ac31bdf693b35f75462da6a0cb66879e8e8b0c96
John Galt <johngaltfirstrun@gmail.com>
2023-01-03 15:58:51 -0500

    sde_connector: fix off by one frame on press hbm for dim

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -623,13 +623,7 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 	if (status == dsi_panel_get_fod_ui(panel))
 		return;
 
-	if (status)
-		sde_encoder_wait_for_event(c_conn->encoder, MSM_ENC_VBLANK);
-
 	dsi_panel_set_fod_hbm(panel, status);
-
-	if (!status)
-		sde_encoder_wait_for_event(c_conn->encoder, MSM_ENC_VBLANK);
 
 	dsi_panel_set_fod_ui(panel, status);
 }
