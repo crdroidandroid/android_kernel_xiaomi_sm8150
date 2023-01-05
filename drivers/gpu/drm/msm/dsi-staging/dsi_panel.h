4af5f2016435105dd873da1bfe156f5e3b43c650
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-05 21:36:08 +0800

    drivers: dsi_panel: prepare for new fod stuff
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -179,12 +179,6 @@ struct drm_panel_esd_config {
 #endif
 };
 
-#define BRIGHTNESS_ALPHA_PAIR_LEN 2
-struct brightness_alpha_pair {
-	u32 brightness;
-	u32 alpha;
-};
-
 #if defined(CONFIG_MACH_XIAOMI_VAYU) || defined(CONFIG_MACH_XIAOMI_NABU)
 struct lockdowninfo_cfg {
 	u8 lockdowninfo[16];
@@ -251,7 +245,6 @@ struct dsi_panel {
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
 
-	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
 
 	bool cphy_esd_check;
@@ -266,6 +259,8 @@ struct dsi_panel {
 
 	struct lockdowninfo_cfg lockdowninfo_read;
 #endif
+	int hbm_mode;
+	bool resend_ea;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -386,9 +381,6 @@ int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
-int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
-
-u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_set_esd_check(struct dsi_panel *panel);
 
@@ -408,5 +400,6 @@ void dsi_panel_doubleclick_enable(bool on);
 
 int dsi_panel_lockdowninfo_param_read(struct dsi_panel *panel);
 #endif
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
