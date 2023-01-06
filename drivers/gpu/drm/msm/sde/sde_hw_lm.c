ccf4fd76183cba489fdee80a5323f2fb923fc61d
RealJohnGalt <johngaltfirstrun@gmail.com>
2023-01-07 00:31:03 +0800

    Revert "drm/msm/sde: Don't clear dim layers when there aren't any applied"
    This reverts commit ba518a3a8a99856d27e9834fc5c5822a61860085.

--- drivers/gpu/drm/msm/sde/sde_hw_lm.c
+++ drivers/gpu/drm/msm/sde/sde_hw_lm.c
@@ -178,16 +178,8 @@ static void sde_hw_lm_clear_dim_layer(struct sde_hw_mixer *ctx)
 	int stage_off, i;
 	u32 reset = BIT(16), val;
 
-	if (!ctx->dim.enabled)
-		return;
-
-	ctx->dim.enabled = false;
 	reset = ~reset;
 	for (i = SDE_STAGE_0; i <= sblk->maxblendstages; i++) {
-		if (!ctx->dim.stage[i])
-			continue;
-
-		ctx->dim.stage[i] = false;
 		stage_off = _stage_offset(ctx, i);
 		if (WARN_ON(stage_off < 0))
 			return;
