b2b084646f3aeacee2d695cdb488cf9223af50b6
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-18 23:24:26 +0800

    display: dsi_panel: Fix unused variable when exposure_adjustment is disabled
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -765,7 +765,9 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 	int bl_dc_min = panel->bl_config.bl_min_level * 2;
+#endif
 	struct dsi_backlight_config *bl = &panel->bl_config;
 
 	if (panel->host_config.ext_bridge_num)
