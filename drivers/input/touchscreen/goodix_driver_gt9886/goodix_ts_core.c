9d0567f820335fb339f9563d41d7bbd6d299a6e3
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-26 15:51:13 +0800

    goodix_driver_gt9886: core: properly use _enabled of new attributes
    This should properly set gesture modes based on whether user has it enabled or not in userspace
    instead of forcing gestures to be on all the time like before
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
@@ -849,6 +849,12 @@ static ssize_t udfps_enabled_store(struct device *dev,
 {
 	struct goodix_ts_core *core_data = dev_get_drvdata(dev);
 	core_data->udfps_enabled = buf[0] != '0';
+
+	core_data->fod_status = core_data->udfps_enabled;
+	core_data->gesture_enabled = core_data->double_wakeup | core_data->fod_status;
+
+	goodix_check_gesture_stat(true);
+
 	return count;
 }
 
@@ -872,6 +878,12 @@ static ssize_t double_tap_enabled_store(struct device *dev,
 {
 	struct goodix_ts_core *core_data = dev_get_drvdata(dev);
 	core_data->double_tap_enabled = buf[0] != '0';
+
+	core_data->double_wakeup = core_data->double_tap_enabled;
+	core_data->gesture_enabled = core_data->double_wakeup | core_data->fod_status;
+
+	goodix_check_gesture_stat(true);
+
 	return count;
 }
 
