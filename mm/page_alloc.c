50b16c0af51826ab7ba6d86a7d3cd60279029e47
onettboots <blackcocopet@gmail.com>
2023-02-25 07:09:50 +0000

    kprofiles: page_alloc: setup each kprofiles boosting
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4089,6 +4089,7 @@ check_retry_cpuset(int cpuset_mems_cookie, struct alloc_context *ac)
 	return false;
 }
 
+extern int kp_active_mode(void);
 static inline struct page *
 __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 						struct alloc_context *ac)
@@ -4210,8 +4211,13 @@ retry:
 		wake_all_kswapds(order, ac);
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
+	if (kp_active_mode() == 2 || kp_active_mode() == 0) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
+	} else if (kp_active_mode() == 3) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
+	}
 
 	reserve_flags = __gfp_pfmemalloc_flags(gfp_mask);
 	if (reserve_flags)
