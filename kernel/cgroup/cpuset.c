1e696d3d9af7ca128247b7b9618261031813eb52
onettboots <blackcocopet@gmail.com>
2023-03-09 17:31:13 +0000

    cpuset: update uclamp tune
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2132,7 +2132,7 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"20", "max", 1, 1},  // 20-100%
+		{"top-app",    	     	"10", "max", 1, 1},  // 10-100%
 		{"foreground", 	     	"10", "80",  1, 0},  // 10-80%
 		{"background", 	     	"0",  "50",  0, 0},  // 0-50%
 		{"system-background", 	"0",  "60",  0, 0},  // 0-60%
