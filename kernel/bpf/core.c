23ff71750e75548be580634a944c6ca308f303e9
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-03-13 22:37:48 +0800

    Revert "bpf: fix subprog verifier bypass by div/mod by 0 exception"
    This reverts commit fe2aac9734770c406210f4826195021f153c1d10.

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1078,10 +1078,14 @@ select_insn:
 		(*(s64 *) &DST) >>= IMM;
 		CONT;
 	ALU64_MOD_X:
+		if (unlikely(SRC == 0))
+			return 0;
 		div64_u64_rem(DST, SRC, &AX);
 		DST = AX;
 		CONT;
 	ALU_MOD_X:
+		if (unlikely((u32)SRC == 0))
+			return 0;
 		AX = (u32) DST;
 		DST = do_div(AX, (u32) SRC);
 		CONT;
@@ -1094,9 +1098,13 @@ select_insn:
 		DST = do_div(AX, (u32) IMM);
 		CONT;
 	ALU64_DIV_X:
+		if (unlikely(SRC == 0))
+			return 0;
 		DST = div64_u64(DST, SRC);
 		CONT;
 	ALU_DIV_X:
+		if (unlikely((u32)SRC == 0))
+			return 0;
 		AX = (u32) DST;
 		do_div(AX, (u32) SRC);
 		DST = (u32) AX;
