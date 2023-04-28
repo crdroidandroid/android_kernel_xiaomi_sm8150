fb01f04fb008ca13d416b01132283a510e0bab3c
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-04-28 10:40:49 +0800

    Revert "BACKPORT: msm: kgsl: Fix possible NULL pointer dereference"
    This reverts commit 4c97811ae1a1cd92ccbf1895f900da4c4bd5b0b0.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5161,12 +5161,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	device->events_wq = alloc_workqueue("kgsl-events",
 		WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_SYSFS | WQ_HIGHPRI, 0);
 
-	if (!device->events_wq) {
-		dev_err(device->dev, "Failed to allocate events workqueue\n");
-		status = -ENOMEM;
-		goto error_pwrctrl_close;
-	}
-
 	/* Initialize the snapshot engine */
 	kgsl_device_snapshot_init(device);
 
@@ -5187,10 +5181,7 @@ EXPORT_SYMBOL(kgsl_device_platform_probe);
 
 void kgsl_device_platform_remove(struct kgsl_device *device)
 {
-	if (device->events_wq) {
-		destroy_workqueue(device->events_wq);
-		device->events_wq = NULL;
-	}
+	destroy_workqueue(device->events_wq);
 
 	kfree(device->dev->dma_parms);
 	device->dev->dma_parms = NULL;
@@ -5217,16 +5208,6 @@ EXPORT_SYMBOL(kgsl_device_platform_remove);
 
 static void kgsl_core_exit(void)
 {
-	if (kgsl_driver.workqueue) {
-		destroy_workqueue(kgsl_driver.workqueue);
-		kgsl_driver.workqueue = NULL;
-	}
-
-	if (kgsl_driver.mem_workqueue) {
-		destroy_workqueue(kgsl_driver.mem_workqueue);
-		kgsl_driver.mem_workqueue = NULL;
-	}
-
 	kgsl_events_exit();
 	kgsl_core_debugfs_close();
 
@@ -5349,20 +5330,8 @@ static int __init kgsl_core_init(void)
 	kgsl_driver.workqueue = alloc_workqueue("kgsl-workqueue",
 		WQ_CPU_INTENSIVE | WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
 
-	if (!kgsl_driver.workqueue) {
-		pr_err("kgsl: Failed to allocate kgsl workqueue\n");
-		result = -ENOMEM;
-		goto err;
-	}
-
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
 		WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_FREEZABLE, 0);
-
-	if (!kgsl_driver.mem_workqueue) {
-		pr_err("kgsl: Failed to allocate mem workqueue\n");
-		result = -ENOMEM;
-		goto err;
-	}
 
 	if (IS_ERR_VALUE(kgsl_run_one_worker_perf(&kgsl_driver.worker,
 			&kgsl_driver.worker_thread,
