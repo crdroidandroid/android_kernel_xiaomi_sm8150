21fcb893978a9a3a4df74ede84b4d3671126099f
LuffyTaro008 <rizal83702@gmail.com>
2023-05-01 16:30:03 +0700

    Revert "msm: kgsl: Place worker thread on SCHED_RR & WQ_HIGHPRI"
    This reverts commit ed8bb69b3839fb178f63f900b51b51cf29317528.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5331,7 +5331,7 @@ static int __init kgsl_core_init(void)
 		WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
 
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
-		WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_FREEZABLE, 0);
+		WQ_MEM_RECLAIM, 0);
 
 	if (IS_ERR_VALUE(kgsl_run_one_worker(&kgsl_driver.worker,
 			&kgsl_driver.worker_thread,
@@ -5341,8 +5341,8 @@ static int __init kgsl_core_init(void)
 			"kgsl_low_prio_worker_thread")))
 		goto err;
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
-	/* kgsl_driver.low_prio_worker_thread should not be SCHED_RR */
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
+	/* kgsl_driver.low_prio_worker_thread should not be SCHED_FIFO */
 
 	kgsl_events_init();
 
