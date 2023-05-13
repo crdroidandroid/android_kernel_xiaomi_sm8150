7a1f3423402e5dada629a3a39331dd899a414285
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-05-13 08:36:47 +0800

    dsi_display: do not set hbm_mode if in autohbm
    If user turns off HBM manually when in autohbm it will turn screen completely off
    Unsure of proper fix

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5127,6 +5127,9 @@ static ssize_t sysfs_hbm_write(struct device *dev,
 	if (!display->panel)
 		return -EINVAL;
 
+        if (display->panel->bl_config.bl_level > 2047)
+                return count;
+
 	ret = kstrtoint(buf, 10, &hbm_mode);
 	if (ret) {
 		pr_debug("kstrtoint failed. ret=%d\n", ret);
