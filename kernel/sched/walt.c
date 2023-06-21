d5cb3267c4e8f31f800a803d89b5f4f91dddb0d6
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-21 23:28:24 +0800

    sched/walt: Resolve mixing declarations and code error from 78e8e64b5c27cbef2d3f430af649219e6d483157
    ../kernel/sched/walt.c:3429:15: error: mixing declarations and code is a C99 extension [-Werror,-Wdeclaration-after-statement]
            unsigned int init_task_load_pct;
                         ^
    1 error generated.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3383,15 +3383,10 @@ int walt_proc_group_thresholds_handler(struct ctl_table *table, int write,
 static void walt_init_once(void)
 {
 	unsigned int init_task_load_pct;
-	init_irq_work(&walt_migration_irq_work, walt_irq_work);
-	init_irq_work(&walt_cpufreq_irq_work, walt_irq_work);
-	walt_rotate_work_init();
 
 	walt_cpu_util_freq_divisor =
 	    (sched_ravg_window >> SCHED_CAPACITY_SHIFT) * 100;
 	walt_scale_demand_divisor = sched_ravg_window >> SCHED_CAPACITY_SHIFT;
-
-	unsigned int init_task_load_pct;
 
 	switch (kp_active_mode()) {
 	case 3:
