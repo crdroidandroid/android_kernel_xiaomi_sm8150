2656aa3cba3f8453db86696df7a0caa91ae1cb67
Andrew Yang <andrew.yang@mediatek.com>
2023-06-30 17:22:02 +0800

    FROMGIT: fs: drop_caches: draining pages before dropping caches
    We expect a file page access after dropping caches should be a major
    fault, but sometimes it's still a minor fault.  That's because a file page
    can't be dropped if it's in a per-cpu pagevec.  Draining all pages from
    per-cpu pagevec to lru list before trying to drop caches.
    
    Link: https://lkml.kernel.org/r/20230630092203.16080-1-andrew.yang@mediatek.com
    Change-Id: I9b03c53e39b87134d5ddd0c40ac9b36cf4d190cd
    Signed-off-by: Andrew Yang <andrew.yang@mediatek.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Bug: 285794522
    (cherry picked from commit a481c6fdf3e4fdf31bda91098dfbf46098037e76
     https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git mm-unstable)
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/drop_caches.c
+++ fs/drop_caches.c
@@ -9,6 +9,7 @@
 #include <linux/writeback.h>
 #include <linux/sysctl.h>
 #include <linux/gfp.h>
+#include <linux/swap.h>
 #include "internal.h"
 
 /* A global variable is a bit ugly, but it keeps the code simple */
@@ -69,7 +70,15 @@ int drop_caches_sysctl_handler(struct ctl_table *table, int write,
 	if (write) {
 		static int stfu;
 
-		mm_drop_caches(sysctl_drop_caches);
+		if (sysctl_drop_caches & 1) {
+			lru_add_drain_all();
+			iterate_supers(drop_pagecache_sb, NULL);
+			count_vm_event(DROP_PAGECACHE);
+		}
+		if (sysctl_drop_caches & 2) {
+			drop_slab();
+			count_vm_event(DROP_SLAB);
+		}
 
 		if (!stfu) {
 			pr_info("%s (%d): drop_caches: %d\n",
