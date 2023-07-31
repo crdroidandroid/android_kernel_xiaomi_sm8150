f41b91ae0015baf22ef2c77938e555f5ae2f9106
kondors1995 <normandija1945@gmail.com>
2023-07-31 17:31:05 +0300

    Revert "kernel: tune_dummy: Add prefer_high_cap node"
    This reverts commit c57fac5de05f39880f94a66caa4dcaaa1d847371.

--- kernel/sched/tune_dummy.c
+++ kernel/sched/tune_dummy.c
@@ -44,19 +44,6 @@ prefer_idle_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	return 0;
 }
 
-static u64
-prefer_high_cap_read(struct cgroup_subsys_state *css, struct cftype *cft)
-{
-	return 0;
-}
-
-static int
-prefer_high_cap_write(struct cgroup_subsys_state *css, struct cftype *cft,
-	    u64 prefer_idle)
-{
-	return 0;
-}
-
 static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
@@ -80,11 +67,6 @@ static struct cftype files[] = {
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
 		.write_u64 = prefer_idle_write,
-	},
-	{
-		.name = "prefer_high_cap",
-		.read_u64 = prefer_high_cap_read,
-		.write_u64 = prefer_high_cap_write,
 	},
 	{ }	/* terminate */
 };
