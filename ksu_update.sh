867d341f1c2db7e6ff66987ddd8d9eae44f1e2c5
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-08-10 07:54:59 +0800

    ksu_update.sh: Commit message for squash commit
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- ksu_update.sh
+++ ksu_update.sh
@@ -2,6 +2,6 @@
 
 echo pulling main branch of KernelSU
 
-git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash
+git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash -m "drivers: kernelsu: update"
 
 echo done
