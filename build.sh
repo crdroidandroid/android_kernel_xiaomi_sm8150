aa3111ba6f23ad00d95bd98ec6756611d165403a
kondors1995 <normandija1945@gmail.com>
2023-11-04 09:38:49 +0200

    build.sh:clang-r498229b

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ restore='\033[0m'
 clear
 
 # Resources
-export CLANG_PATH=~/tc/neutron-clang/bin
+export CLANG_PATH=~/tc/aosp/clang-r498229b/bin
 export PATH=${CLANG_PATH}:${PATH}
 export THINLTO_CACHE=~/ltocache/
 DEFCONFIG="raphael_defconfig"
