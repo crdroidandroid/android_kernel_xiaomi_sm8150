af89085a588777d65900a2ae143e3818dc9d18e5
John Galt <johngaltfirstrun@gmail.com>
2023-11-15 14:33:17 -0500

    sbalance: balance on fork
    There are often new load and capacity requirements
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2018-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
@@ -15,7 +15,7 @@ enum df_device {
 
 #ifdef CONFIG_DEVFREQ_BOOST
 void devfreq_boost_kick(enum df_device device);
-void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
+void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms, bool always);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
 #else
 static inline
@@ -23,7 +23,7 @@ void devfreq_boost_kick(enum df_device device)
 {
 }
 static inline
-void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
+void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms, bool always)
 {
 }
 static inline
--- kernel/fork.c
+++ kernel/fork.c
@@ -96,7 +96,7 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/devfreq_boost.h>
-#include <linux/cpu_input_boost.h>
+#include <linux/irq.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2261,8 +2261,9 @@ long _do_fork(unsigned long clone_flags,
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(500, false);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 700);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 700);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500, true);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500, true);
+		balance_irqs();
 	}
 
 	/*
