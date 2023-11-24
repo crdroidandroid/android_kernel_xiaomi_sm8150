0323eaaa064e35ec9be12657a2ff03c662290338
onettboots <blackcocopet@gmail.com>
2023-11-24 13:40:13 +0000

    mm: tune vm and cache dirty_ration = 50 vfs_cache_pressure = 50 extra_free_kbytes = 29615 min_free_kbytes = 9456 adapted from Waffles kernel
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -321,16 +321,16 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 16384;
+int min_free_kbytes = 9456;
 int user_min_free_kbytes = -1;
-int watermark_scale_factor = 10;
+int watermark_scale_factor = 32;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
  * free memory, to make space for new workloads. Anyone can allocate
  * down to the min watermarks controlled by min_free_kbytes above.
  */
-int extra_free_kbytes = 0;
+int extra_free_kbytes = 29615;
 
 static unsigned long __meminitdata nr_kernel_pages;
 static unsigned long __meminitdata nr_all_pages;
