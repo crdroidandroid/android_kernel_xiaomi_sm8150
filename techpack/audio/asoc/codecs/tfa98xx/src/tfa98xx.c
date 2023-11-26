33c4e2e7cef771c8d3da51f407a1bdcd123edefa
PIPIPIG233666 <pig.priv@gmail.com>
2023-11-26 13:48:18 +0100

    techpack: audio: Promote send_tfa_cal_apr out of debugfs codes
    Change-Id: I95be412e79dfdd9e29785ac1022fcc7664cdcdff
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -391,6 +391,15 @@ static void tfa98xx_inputdev_unregister(struct tfa98xx *tfa98xx)
 	__tfa98xx_inputdev_check_register(tfa98xx, true);
 }
 
+#ifdef TFA_NON_DSP_SOLUTION
+extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
+#else
+int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
+{
+    return 0;
+}
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 /* OTC reporting
  * Returns the MTP0 OTC bit value
@@ -741,15 +750,6 @@ static ssize_t tfa98xx_dbgfs_fw_state_get(struct file *file,
 
 	return simple_read_from_buffer(user_buf, count, ppos, str, strlen(str));
 }
-
-#ifdef TFA_NON_DSP_SOLUTION
-extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
-#else
-int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
-{
-    return 0;
-}
-#endif
 
 static ssize_t tfa98xx_dbgfs_rpc_read(struct file *file,
 				     char __user *user_buf, size_t count,
