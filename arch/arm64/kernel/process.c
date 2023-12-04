ee692180fcfeb9c8b7e8f3313d09a0dfea37c0e7
Sultan Alsawaf <sultan@kerneltoast.com>
2023-12-03 18:01:22 -0800

    arm64: Add a timer to cpu_do_idle() to avoid staying in WFI too long
    It is often the case that a CPU may remain in a shallow idle state longer
    than the cpuidle governor originally predicted, leading to lost energy
    savings had the CPU been placed into a deeper idle state.
    
    The cpuidle governors lack a shallow idle state timeout, meaning that a CPU
    could remain stuck in WFI indefinitely. Furthermore, it is difficult to
    implement such a timeout in a generic way, since it is innately tied to CPU
    architectural details.
    
    Therefore, implement an idle timeout in arm64 using TEO in order to improve
    CPU idle energy savings significantly. A timeout of 1 ms is empirically
    found to provide the best energy saving benefit, and should be applicable
    to most arm64 CPUs since WFI exit latency is generally quite low.
    
    On Tensor G3, this consistently reduces CPU + RAM power consumption by at
    least 30 mW in a light gaming load scenario (Temple Run 2).
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -69,6 +69,9 @@ __visible unsigned long __stack_chk_guard  __ro_after_init;;
 EXPORT_SYMBOL(__stack_chk_guard);
 #endif
 
+static DEFINE_PER_CPU(struct hrtimer, wfi_timer);
+s64 teo_wfi_timeout_ns(void);
+
 /*
  * Function pointers to optional machine specific functions
  */
@@ -111,8 +114,39 @@ static void __cpu_do_idle_irqprio(void)
  */
 void cpu_do_idle(void)
 {
+	s64 wfi_timeout_ns = teo_wfi_timeout_ns();
+	struct hrtimer *timer = NULL;
+
+	/*
+	 * If the tick is stopped, arm a timer to ensure that the CPU doesn't
+	 * stay in WFI too long and burn power. That way, the CPU will be woken
+	 * up so it can enter a deeper idle state instead of staying in WFI.
+	 */
+	if (wfi_timeout_ns) {
+		/* Use TEO's estimated sleep duration with some slack added */
+		timer = this_cpu_ptr(&wfi_timer);
+		hrtimer_start(timer, ns_to_ktime(wfi_timeout_ns),
+			      HRTIMER_MODE_REL_PINNED_HARD);
+	}
+
 	__cpu_do_idle();
+
+	/* Cancel the timer if it was armed. This always succeeds. */
+	if (timer)
+		hrtimer_try_to_cancel(timer);
 }
+
+static int __init wfi_timer_init(void)
+{
+	int cpu;
+
+	/* No function is needed; the timer is canceled while IRQs are off */
+	for_each_possible_cpu(cpu)
+		hrtimer_init(&per_cpu(wfi_timer, cpu), CLOCK_MONOTONIC,
+			     HRTIMER_MODE_REL_HARD);
+	return 0;
+}
+pure_initcall(wfi_timer_init);
 
 /*
  * This is our default idle handler.
