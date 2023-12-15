b1143b6d6cccc4794a337a3fb45b077fa0704712
onettboots <blackcocopet@gmail.com>
2023-12-15 20:39:28 +0000

    Revert "sched/walt: Resolve mixing declarations and code error from 78e8e64b5c27cbef2d3f430af649219e6d483157"
    This reverts commit cd9f9a5ebee2f48294bd8d19bf70e69b74da1076.

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3398,11 +3398,11 @@ int walt_proc_group_thresholds_handler(struct ctl_table *table, int write,
 
 static void walt_init_once(void)
 {
-	unsigned int init_task_load_pct;
-
 	walt_cpu_util_freq_divisor =
 	    (sched_ravg_window >> SCHED_CAPACITY_SHIFT) * 100;
 	walt_scale_demand_divisor = sched_ravg_window >> SCHED_CAPACITY_SHIFT;
+
+	unsigned int init_task_load_pct;
 
 	switch (kp_active_mode()) {
 	case 3:
