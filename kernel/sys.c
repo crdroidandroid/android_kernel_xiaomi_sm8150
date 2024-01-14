abecb44ad68a99aba9d6829c05fb59813d38000a
Tim Zimmermann <tim@linux4.de>
2024-01-14 05:49:48 +0100

    syscall: Increase bpf fake uname to 5.4
    Change-Id: I50bfa0d35d81f1c8cc21530ea0524a6752d0d34c
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1215,7 +1215,7 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "4.19.0");
+		strcpy(tmp.release, "5.4.0");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
