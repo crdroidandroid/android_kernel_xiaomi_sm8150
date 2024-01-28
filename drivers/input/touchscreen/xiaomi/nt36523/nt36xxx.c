53cc772f68642b6de1af39c1676a01409946ad7e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-01-28 16:22:06 +0100

    drivers: input: touchscreen: xiaomi: nt36523: Implement double_tap node
    Change-Id: Icaa46478fac2293a9c89a464f9975c679d7b3f4f

--- drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.c
+++ drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.c
@@ -48,6 +48,10 @@
 #include "../xiaomi/xiaomi_touch.h"
 #endif
 
+#if WAKEUP_GESTURE && defined(CONFIG_TOUCHSCREEN_COMMON)
+#include <linux/input/tp_common.h>
+#endif
+
 #if NVT_TOUCH_ESD_PROTECT
 static struct delayed_work nvt_esd_check_work;
 static struct workqueue_struct *nvt_esd_check_wq;
@@ -119,6 +123,31 @@ const uint16_t gesture_key_array[] = {
 	KEY_POWER, //GESTURE_SLIDE_RIGHT
 	KEY_WAKEUP, //GESTURE_PEN_ONE_CLICK
 };
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+			       struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", ts->db_wakeup);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+				struct kobj_attribute *attr, const char *buf,
+				size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	ts->db_wakeup = !!val;
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = { .show = double_tap_show,
+					       .store = double_tap_store };
+#endif
 #endif
 
 #ifdef CONFIG_MTK_SPI
@@ -3024,6 +3053,14 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 		input_set_capability(ts->input_dev, EV_KEY,
 				     gesture_key_array[retry]);
 	}
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (ret < 0) {
+		NVT_ERR("%s: Failed to create double_tap node err=%d\n",
+			__func__, ret);
+	}
+#endif
 #endif
 
 	sprintf(ts->phys, "input/ts");
