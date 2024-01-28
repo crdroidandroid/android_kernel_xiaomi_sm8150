e0f663230d1b8a84309ebe69cb25937dd422bc08
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-01-28 03:09:02 +0100

    drivers: input: touchscreen: xiaomi: Drop lpm_disable_for_input
    Change-Id: I3b8c72c63dc3e20c00e54ef28925126345075a38

--- drivers/input/touchscreen/xiaomi/fts_521/fts.c
+++ drivers/input/touchscreen/xiaomi/fts_521/fts.c
@@ -152,7 +152,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force);
 static int fts_chip_initialization(struct fts_ts_info *info, int init_type);
 static const char *fts_get_limit(struct fts_ts_info *info);
 static irqreturn_t fts_event_handler(int irq, void *ts_info);
-extern void lpm_disable_for_input(bool on);
 
 /**
 * Release all the touches in the linux input subsystem
@@ -183,7 +182,6 @@ void release_all_touches(struct fts_ts_info *info)
 #ifdef STYLUS_MODE
 	info->stylus_id = 0;
 #endif
-	lpm_disable_for_input(false);
 }
 
 /**
@@ -3397,7 +3395,6 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 	input_mt_report_slot_state(info->input_dev, tool, 0);
 	if (info->touch_id == 0) {
 		input_report_key(info->input_dev, BTN_TOUCH, touch_condition);
-		lpm_disable_for_input(false);
 		if (!touch_condition)
 			input_report_key(info->input_dev, BTN_TOOL_FINGER, 0);
 #ifdef CONFIG_FTS_FOD_AREA_REPORT
@@ -4118,7 +4115,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 				tag);
 			pm_relax(info->dev);
 			fts_enableInterrupt();
-			lpm_disable_for_input(false);
 			return;
 		} else {
 			logError(
@@ -4178,7 +4174,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 #endif
 	pm_relax(info->dev);
 	fts_enableInterrupt();
-	lpm_disable_for_input(false);
 	return;
 }
 
@@ -4216,8 +4211,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 		return IRQ_HANDLED;
 	}
 #endif
-	lpm_disable_for_input(true);
-
 	info->irq_status = true;
 	error = fts_writeReadU8UX(regAdd, 0, 0, data, FIFO_EVENT_SIZE,
 				  DUMMY_FIFO);
@@ -4263,8 +4256,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 	}
 	input_sync(info->input_dev);
 	info->irq_status = false;
-	if (!info->touch_id)
-		lpm_disable_for_input(false);
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 	wake_up(&info->wait_queue);
 #endif
@@ -5780,7 +5771,6 @@ static void fts_suspend_work(struct work_struct *work)
 	sysfs_notify(&fts_info->fts_touch_dev->kobj, NULL,
 		     "touch_suspend_notify");
 #endif
-	lpm_disable_for_input(false);
 }
 
 #ifdef CONFIG_DRM
