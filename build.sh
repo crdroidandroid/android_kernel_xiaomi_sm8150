cf5cd461270c334d8bdc1ec30e568dd1c756bcf0
onettboots <blackcocopet@gmail.com>
2024-01-31 16:04:25 +0000

    Boolx-1.5
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export SUBARCH=arm64
 export ARCH=arm64
 export CC=$HOME/toolchains/boolx-clang/bin/clang
 
-VER="V1.4-junkidding-DSP"
+VER="V1.5-Pickacak-DSP"
 KERNEL_DIR=`pwd`
 REPACK_DIR=$HOME/AnyKernel3
 ZIP_MOVE=$HOME/Boolx
