3a21b1231c5e0ecead09e053976613b8b3c75b91
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-07 08:16:36 -0800

    kernel/sys.c: Strip localversion from uname for GMS processes
    Play Integrity now hashes the kernel's localversion and compares it to a
    blacklist of localversion hashes for the purpose of failing Play Integrity
    attestation.
    
    Among the banned localversion hashes are "-Sultan" and "-sultan".
    
    Since this is biased and doesn't improve security for end users in any way,
    strip the localversion from uname for processes which are part of GMS's
    thread group so that Play Integrity doesn't fail due to this.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1207,6 +1207,8 @@ static int override_version(struct new_utsname __user *name)
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
+	struct task_struct *t;
+	bool is_gms = false;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
@@ -1218,6 +1220,21 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 			 current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
+
+	rcu_read_lock();
+	for_each_thread(current, t) {
+		if (thread_group_leader(t)) {
+			is_gms = !strcmp(t->comm, "id.gms.unstable");
+			break;
+		}
+	}
+	rcu_read_unlock();
+
+	if (is_gms)
+		snprintf(tmp.release, sizeof(tmp.release), "%u.%u.%u",
+			 LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL,
+			 LINUX_VERSION_SUBLEVEL);
+
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
