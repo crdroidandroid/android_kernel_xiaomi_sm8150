185b98562b121d51fda4f38798f6bd7b8f9d7a4f
Kanonify <152477491+kanon-ify@users.noreply.github.com>
2024-02-10 00:51:42 +0000

    simple_gpu: increase ramp up and decrease laziness
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -19,10 +19,10 @@
 #include <linux/devfreq.h>
 #include <linux/msm_adreno_devfreq.h>
 
-static int default_laziness = 4;
+static int default_laziness = 3;
 module_param_named(simple_laziness, default_laziness, int, 0664);
 
-static int ramp_up_threshold = 5000;
+static int ramp_up_threshold = 6000;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
 int simple_gpu_active = 1;
