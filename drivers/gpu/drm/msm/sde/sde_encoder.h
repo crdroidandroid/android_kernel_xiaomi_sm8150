7cbd98144c08e6da3b2b6f7c797ea3c274a4a635
Pranav Vashi <neobuddy89@gmail.com>
2024-02-24 13:33:59 +0530

    Revert "drm/msm/sde: use first vblank event to trigger early wakeup"
    This reverts commit aa43323d7b08dbe663ec6aa19b9aa31a64b890c5.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_encoder.h
+++ drivers/gpu/drm/msm/sde/sde_encoder.h
@@ -346,9 +346,10 @@ void sde_encoder_control_idle_pc(struct drm_encoder *enc, bool enable);
 int sde_encoder_in_cont_splash(struct drm_encoder *enc);
 
 /**
- * sde_encoder_trigger_early_wakeup - trigger early wake up
+ * sde_encoder_get_ctlstart_timeout_state - checks if ctl start timeout happened
  * @drm_enc:    Pointer to drm encoder structure
+ * @Return:     non zero value if ctl start timeout occurred
  */
-void sde_encoder_trigger_early_wakeup(struct drm_encoder *drm_enc);
+int sde_encoder_get_ctlstart_timeout_state(struct drm_encoder *enc);
 
 #endif /* __SDE_ENCODER_H__ */
