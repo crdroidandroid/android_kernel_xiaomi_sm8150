c44352521787a235d52a90e94efad685f1effda9
chiteroman <98092901+chiteroman@users.noreply.github.com>
2024-03-04 14:07:36 +0100

    drivers: Add KernelSU hooks
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1738,6 +1738,14 @@ static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 }
 #endif
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1751,6 +1759,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
