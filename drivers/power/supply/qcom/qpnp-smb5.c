78d407baa14c6cd2a108364ed912821d52442815
onettboots <blackcocopet@gmail.com>
2024-03-18 13:11:58 +0000

    Revert "qcom: qpnp-smb5: Expose system temperature level properties"
    This reverts commit 09c982d450106cfd6ad2dd2ae217fb3c16e973e2.

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2209,7 +2209,6 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 	POWER_SUPPLY_PROP_DC_THERMAL_LEVELS,
-	POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL,
 };
 
 #define DEBUG_ACCESSORY_TEMP_DECIDEGC	250
@@ -2238,6 +2237,9 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		rc = smblib_get_prop_batt_capacity(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
+		rc = smblib_get_prop_system_temp_level(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
 		rc = smblib_get_prop_system_temp_level(chg, val);
@@ -2377,18 +2379,6 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_DYNAMIC_FV_ENABLED:
 		val->intval = chg->dynamic_fv_enabled;
 		break;
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED:
-		rc = smblib_get_prop_battery_charging_enabled(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_LIMITED:
-		rc = smblib_get_prop_battery_charging_limited(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_SLOWLY_CHARGING:
-		rc = smblib_get_prop_battery_slowly_charging(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_BQ_INPUT_SUSPEND:
-		rc = smblib_get_prop_battery_bq_input_suspend(chg, val);
-		break;
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
@@ -2425,6 +2415,9 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		rc = smblib_set_prop_batt_capacity(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
+		rc = smblib_set_prop_system_temp_level(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_PARALLEL_DISABLE:
 		vote(chg->pl_disable_votable, USER_VOTER, (bool)val->intval, 0);
@@ -2500,42 +2493,6 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		chg->fcc_stepper_enable = val->intval;
-		break;
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED:
-		if (chg->use_bq_pump) {
-			if (val->intval == 0)
-				vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-							true, MAIN_CHARGER_STOP_ICL);
-			else
-				vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-							false, 0);
-			rerun_election(chg->usb_icl_votable);
-		}
-		break;
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_LIMITED:
-		if (chg->use_bq_pump) {
-			if (val->intval == 0) {
-				vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-							false, MAIN_CHARGER_ICL);
-			} else {
-				if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB_HVDCP_3)
-					vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-								true, QC3_CHARGER_ICL);
-				else if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB_HVDCP_3P5)
-					vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-								true, QC3P5_CHARGER_ICL);
-				else
-					vote(chg->usb_icl_votable, MAIN_CHG_VOTER,
-								true, MAIN_CHARGER_ICL);
-			}
-			rerun_election(chg->usb_icl_votable);
-		}
-		break;
-	case POWER_SUPPLY_PROP_SLOWLY_CHARGING:
-			rc = smblib_set_prop_battery_slowly_charging(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = smblib_set_prop_system_temp_level(chg, val);
 		break;
 	default:
 		rc = -EINVAL;
