5d04a850079398359911123d76598d09fc30c27b
kondors1995 <normandija1945@gmail.com>
2024-04-04 12:48:06 +0300

    sched/cass:checkout to kerneltoast/android_kernel_google_zuma@63f0b82d3

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -184,12 +184,12 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 }
 
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
-				    int sd_flag, int wake_flags)
+				    int wake_flags)
 {
 	bool sync;
 
 	/* Don't balance on exec since we don't know what @p will look like */
-	if (sd_flag & SD_BALANCE_EXEC)
+	if (wake_flags & SD_BALANCE_EXEC)
 		return prev_cpu;
 
 	/*
@@ -201,7 +201,7 @@ static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
 		return cpumask_first(p->cpus_ptr);
 
 	/* cass_best_cpu() needs the task's utilization, so sync it up */
-	if (!(sd_flag & SD_BALANCE_FORK))
+	if (!(wake_flags & SD_BALANCE_FORK))
 		sync_entity_load_avg(&p->se);
 
 	sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
