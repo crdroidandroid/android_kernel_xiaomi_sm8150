7390f693d8b62b21a074a7e5974cf8523190034b
onettboots <blackcocopet@gmail.com>
2024-04-06 20:09:30 +0000

    Revert "cpufreq: schedutil: Extend limit tunables to prime"
    This reverts commit 2ed2468b6dcf1b96c5ff283e8bc6edae5cc5b8eb.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1052,13 +1052,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
-	if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-		tunables->up_rate_limit_us =
-					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_PRIME;
-		tunables->down_rate_limit_us =
-					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_PRIME;
-	}
-
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
 		tunables->up_rate_limit_us =
 					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
