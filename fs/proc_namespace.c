a69a3764e85f1d49e193be4909e6e974581d7976
onettboots <blackcocopet@gmail.com>
2024-04-06 20:00:22 +0000

    Revert "fs: prevent Zygisk/KernelSU detection through common methods"
    This reverts commit 65494ec26d976eba87c202f30860a4b46c9775d0.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -12,7 +12,6 @@
 #include <linux/security.h>
 #include <linux/fs_struct.h>
 #include <linux/sched/task.h>
-#include <linux/suspicious.h>
 
 #include "proc/internal.h" /* only for get_proc_task() in ->open() */
 
@@ -103,11 +102,6 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
 
-	if (is_suspicious_mount(mnt, &p->root)) {
-		err = SEQ_SKIP;
-		goto out;
-	}
-
 	if (sb->s_op->show_devname) {
 		err = sb->s_op->show_devname(m, mnt_path.dentry);
 		if (err)
@@ -143,11 +137,6 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt->mnt_sb;
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	int err;
-
-	if (is_suspicious_mount(mnt, &p->root)) {
-		err = SEQ_SKIP;
-		goto out;
-	}
 
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
 		   MAJOR(sb->s_dev), MINOR(sb->s_dev));
@@ -212,11 +201,6 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
-
-	if (is_suspicious_mount(mnt, &p->root)) {
-		err = SEQ_SKIP;
-		goto out;
-	}
 
 	/* device */
 	if (sb->s_op->show_devname) {
