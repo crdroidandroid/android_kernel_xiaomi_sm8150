6dd270352f3935570a7396802532e86c0671f7d1
onettboots <blackcocopet@gmail.com>
2024-05-11 08:26:16 +0000

    mm: tune up for mm and cache presure
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -319,10 +319,9 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 9456;
+int min_free_kbytes = 1024;
 int user_min_free_kbytes = -1;
-int watermark_scale_factor = 32;
-
+int watermark_scale_factor = 100;
 /*
  * Extra memory for the system to try freeing. Used to temporarily
  * free memory, to make space for new workloads. Anyone can allocate
