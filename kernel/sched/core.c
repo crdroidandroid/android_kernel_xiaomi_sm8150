3627b2b5556fabc2b9666ad1cae70aff72bb04db
John Galt <johngaltfirstrun@gmail.com>
2024-06-09 20:57:37 -0400

    sched/core: decrease TA boost
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -976,10 +976,15 @@ static inline void uclamp_boost_write(struct task_struct *p) {
 
 	//top-app min clamp input boost
 	if (strcmp(css->cgroup->kn->name, "top-app") == 0) {
-		if (kp_active_mode() == 3 || time_before(jiffies, last_input_time + msecs_to_jiffies(7000))) {
+		if (kp_active_mode() == 3) {
 			task_group(p)->uclamp[UCLAMP_MIN].value = 512;
-		} else {
+			task_group(p)->latency_sensitive = 1;
+		} else if (time_before(jiffies, last_input_time + msecs_to_jiffies(3500))) {
 			task_group(p)->uclamp[UCLAMP_MIN].value = 358;
+			task_group(p)->latency_sensitive = 1;
+		} else {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 154;
+			task_group(p)->latency_sensitive = 0;
 		}
 	}
 #ifdef CONFIG_STOCKISH_ROM_SUPPORT
