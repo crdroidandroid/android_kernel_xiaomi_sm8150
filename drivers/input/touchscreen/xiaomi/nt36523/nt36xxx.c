d4e9157c31c5c66592b0ff4c14ede459188ee7ad
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-11 23:25:40 +0200

    drivers: input: touchscreen: xiaomi: nt36523: Implement pen node
    Change-Id: I7dd9174f7e2d9bfd5e1f3d2a43ed4df23f92fa00

--- drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.c
+++ drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.c
@@ -2100,6 +2100,35 @@ static void nvt_pen_charge_state_change_work(struct work_struct *work)
 	disable_pen_input_device(ts->pen_is_charge);
 }
 
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t pen_show(struct kobject *kobj, struct kobj_attribute *attr,
+			char *buf)
+{
+	return sprintf(buf, "%d\n", ts->pen_input_dev_enable);
+}
+
+static ssize_t pen_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	ts->pen_input_dev_enable = !!val;
+	disable_pen_input_device(!ts->pen_input_dev_enable);
+	release_pen_event();
+
+	return count;
+}
+
+static struct tp_common_ops pen_ops = {
+	.show = pen_show,
+	.store = pen_store,
+};
+#endif
+
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 static struct xiaomi_touch_interface xiaomi_touch_interfaces;
 
@@ -3134,6 +3163,14 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 				ts->pen_input_dev->name, ret);
 			goto err_pen_input_register_device_failed;
 		}
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+		ret = tp_common_set_pen_ops(&pen_ops);
+		if (ret < 0) {
+			NVT_ERR("%s: Failed to create pen node err=%d\n",
+				__func__, ret);
+		}
+#endif
 	} /* if (ts->pen_support) */
 
 	//---set int-pin & request irq---
