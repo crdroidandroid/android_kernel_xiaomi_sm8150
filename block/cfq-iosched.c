7018b041608c495d6d6e1d9db74fc9b60206fae3
shygosh <shygosh@proton.me>
2024-07-07 15:28:18 +0700

    iosched: cfq: Apply RedHat tuning suggestion for fast storage
    Signed-off-by: shygosh <shygosh@proton.me>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -32,11 +32,11 @@ static const int cfq_back_penalty = 1;
 static const u64 cfq_slice_sync = NSEC_PER_SEC / 10;
 static u64 cfq_slice_async = NSEC_PER_SEC / 25;
 static const int cfq_slice_async_rq = 2;
-static u64 cfq_slice_idle = NSEC_PER_SEC / 125;
-static u64 cfq_group_idle = 1;
+static u64 cfq_slice_idle = 0;
+static u64 cfq_group_idle = USEC_PER_SEC;
 /* IOPP-cfq_rt_idle_only-v1.0.k4.19 */
 static int cfq_rt_idle_only = 0;
-static const u64 cfq_target_latency = (u64)NSEC_PER_SEC * 3/10; /* 300 ms */
+static const u64 cfq_target_latency = (u64)NSEC_PER_SEC * 1/10; /* 100 ms */
 static const int cfq_hist_divisor = 4;
 /* IOPP-cfq_max_async_dispatch-v1.0.4.4 */
 static int cfq_max_async_dispatch = 8;
