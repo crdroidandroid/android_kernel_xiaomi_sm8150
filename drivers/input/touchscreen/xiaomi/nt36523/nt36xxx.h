e43f9fe03b2715623a6bd5ca0aedbee2eb67750b
stic-server-open <1138705738@qq.com>
2026-01-04 20:52:25 +0800

    drivers: nt36523: Disable NVT_TOUCH_ESD_PROTECT Feature
    * When using the new touchpanel firmware, applying certain screen
      protectors may cause screen touch issues due to frequent ESD checks.
      xiaomi has disable this feature into Xiaomi Pad 6 (pipa) nt36532
    
    Change-Id: Ib5f24c6cfb5237bc0efa58a32088e6c872d63f37

--- drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.h
+++ drivers/input/touchscreen/xiaomi/nt36523/nt36xxx.h
@@ -113,7 +113,7 @@ extern const uint16_t gesture_key_array[];
 #define CHECK_PEN_DATA_CHECKSUM 0
 
 //---ESD Protect.---
-#define NVT_TOUCH_ESD_PROTECT 1
+#define NVT_TOUCH_ESD_PROTECT 0
 #define NVT_TOUCH_ESD_CHECK_PERIOD 1500 /* ms */
 #define NVT_TOUCH_WDT_RECOVERY 1
 
