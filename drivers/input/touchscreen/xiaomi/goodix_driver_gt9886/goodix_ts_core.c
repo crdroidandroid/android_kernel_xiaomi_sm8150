11d49e6e013c726754162a0da0d08e0640a8bcd9
firebird11 <hbgassel@gmail.com>
2026-04-12 01:19:17 +0200

    input: touchscreen: goodix: decouple udfps_enabled from gesture mode
    Keep udfps_enabled sysfs node for HAL, but stop updating gesture mode from it.
    
    Fixes UDFPS enrollment issues (too fast / early release) on raphael.
    
    Change-Id: Id76d527718875f6182d57223cebeec754c84b8de
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/input/touchscreen/xiaomi/goodix_driver_gt9886/goodix_ts_core.c
+++ drivers/input/touchscreen/xiaomi/goodix_driver_gt9886/goodix_ts_core.c
@@ -782,10 +782,6 @@ static ssize_t udfps_enabled_store(struct device *dev,
 	struct goodix_ts_core *core_data = dev_get_drvdata(dev);
 	core_data->udfps_enabled = buf[0] != '0';
 
-	core_data->gesture_enabled = core_data->double_tap_enabled | core_data->udfps_enabled | core_data->single_tap_enabled;
-
-	goodix_check_gesture_stat(true);
-
 	return count;
 }
 
