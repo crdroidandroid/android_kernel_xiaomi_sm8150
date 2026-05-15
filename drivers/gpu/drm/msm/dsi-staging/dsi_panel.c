b352552d3a0e13221d8c6f3d6004002d0c919412
onettboots <blackcocopet@gmail.com>
2026-05-15 16:33:46 +0700

    display: complete HBM impl
    Change-Id: Ide9701c0213aa063e0f303f22b03844356e9ebf6
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1231,7 +1231,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	int rc = 0;
 	int bl_dc_min = panel->bl_config.bl_min_level * 2;
 	struct dsi_backlight_config *bl = &panel->bl_config;
-	u32 last_backlight = dsi_panel_get_backlight(panel);
 
 	if (panel->host_config.ext_bridge_num)
 		return 0;
@@ -5272,6 +5271,10 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	else
 		panel->panel_initialized = true;
 	mutex_unlock(&panel->panel_lock);
+
+	if (panel->hbm_mode)
+                dsi_panel_apply_hbm_mode(panel);
+
 	return rc;
 }
 
@@ -5427,4 +5430,27 @@ int dsi_panel_post_unprepare(struct dsi_panel *panel)
 error:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
+}
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
+{
+        static const enum dsi_cmd_set_type type_map[] = {
+                DSI_CMD_SET_DISP_HBM_FOD_OFF,
+                DSI_CMD_SET_DISP_HBM_FOD_ON
+        };
+
+        enum dsi_cmd_set_type type;
+        int rc;
+
+        if (panel->hbm_mode >= 0 &&
+                panel->hbm_mode < ARRAY_SIZE(type_map))
+                type = type_map[panel->hbm_mode];
+        else
+                type = type_map[0];
+
+        mutex_lock(&panel->panel_lock);
+        rc = dsi_panel_tx_cmd_set(panel, type);
+        mutex_unlock(&panel->panel_lock);
+
+        return rc;
 }
