386d1984b4a63cfa69f710426c426947ddeac52d
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-06-21 15:42:00 +0800

    kernel: More code improvement and bug fixes for SUS_MAP
    - Change to hide all sus map entries as well as the statistics from maps instead of spoofing them as anon entries, since it is futile against prctl syscall passed with the cmd PR_SET_VMA, though normally apps unlikely test each anon entry, but it still worth doing so and at least it can save us some cpu cycles.
    
    - Check before each smap_gather_stats() calls and skip incrementing the mss if matched entry is found.
    
    - In pagemap_read(), instead of setting pm.buffer->pm to 0, we should check the vma fist before calling walk_page_range(), and skip walking the page and writing to pm if matched entry is found.
    
    - To be more efficient, we hook __access_remote_vm() instead of mem_rw().
    
    - There are still various syscalls that can be used to detect the spoofing, but it should be enough for most of the cases for now.
    
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -793,9 +793,6 @@ static ssize_t mem_rw(struct file *file, char __user *buf,
 	ssize_t copied;
 	char *page;
 	unsigned int flags;
-#ifdef CONFIG_KSU_SUSFS_SUS_MAP
-	struct vm_area_struct *vma;
-#endif
 
 	if (!mm)
 		return 0;
@@ -812,20 +809,6 @@ static ssize_t mem_rw(struct file *file, char __user *buf,
 
 	while (count > 0) {
 		size_t this_len = min_t(size_t, count, PAGE_SIZE);
-#ifdef CONFIG_KSU_SUSFS_SUS_MAP
-		vma = find_vma(mm, addr);
-		if (vma && vma->vm_file) {
-			struct inode *inode = file_inode(vma->vm_file);
-			if (SUSFS_IS_INODE_SUS_MAP(inode)) {
-				if (write) {
-					copied = -EFAULT;
-				} else {
-					copied = -EIO;
-				}
-				break;
-			}
-		}
-#endif
 		if (write && copy_from_user(page, buf, this_len)) {
 			copied = -EFAULT;
 			break;
@@ -2356,9 +2339,6 @@ proc_map_files_readdir(struct file *file, struct dir_context *ctx)
 	struct map_files_info info;
 	struct map_files_info *p;
 	int ret;
-#ifdef CONFIG_KSU_SUSFS_SUS_MAP
-	struct inode *inode;
-#endif
 
 	ret = -ENOENT;
 	task = get_proc_task(file_inode(file));
@@ -2412,8 +2392,7 @@ proc_map_files_readdir(struct file *file, struct dir_context *ctx)
 			if (!vma->vm_file)
 				continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
-			inode = file_inode(vma->vm_file);
-			if (SUSFS_IS_INODE_SUS_MAP(inode))
+			if (SUSFS_IS_INODE_SUS_MAP(file_inode(vma->vm_file)))
 				continue;
 #endif
 			if (++pos <= ctx->pos)
